360d36fb94f5fab35b6a77d0d94d0c5b2e270417
arunvinu <96110638+arunvinu@users.noreply.github.com>
2021-12-22 18:07:24 +0530

    Add files via upload

---
+++ Januaryy.java
@@ -0,0 +1,9 @@
+
+class Januaryy{
+public static void main(String args[ ]){
+String [ ]month = {"jan", "feb", "mar", "apr", "may", "jun", "july", "aug", "sep", "oct", "nov", "dec" };
+for (int demo = 0; demo < month.length; demo= demo+1){ 
+System.out.println(month[ demo ]);
+}
+}
+}
