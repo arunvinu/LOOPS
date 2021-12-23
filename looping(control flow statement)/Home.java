01f6942910ca5e491555137a94a131b017428612
arunvinu <96110638+arunvinu@users.noreply.github.com>
2021-12-23 09:43:27 +0545

    Add files via upload

---
+++ looping(control flow statement)/Home.java
@@ -0,0 +1,7 @@
+class Home{
+public static void main(String args[ ]){
+String [ ]serial = {"book","fan","pen","lap","rubber","wires","keys","glasse","kites"};
+for(int a=0;a<=serial.length;a=a+1)
+System.out.println(serial[a]);
+}
+}
